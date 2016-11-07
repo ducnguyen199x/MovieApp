b3b229815968636938f96ed3474b22226f3f5f6f
Nguyen Thanh Duc <DucDP@georges-macbook-air.local>
2016-11-07 09:47:19 +0700

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Nguyen Thanh Duc on 11/7/16.
+//  Copyright © 2016 NguyenThanhDuc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
