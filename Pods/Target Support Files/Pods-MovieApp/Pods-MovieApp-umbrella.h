bf1490ec92c536d4f7595dcc46ef3141e2168b57
Nguyen Thanh Duc <DucDP@Georges-MacBook-Air.local>
2016-11-11 13:16:47 +0700

    rm Pods

--- Pods/Target Support Files/Pods-MovieApp/Pods-MovieApp-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_MovieAppVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_MovieAppVersionString[];
-
--- Pods/Target Support Files/Pods-MovieAppTests/Pods-MovieAppTests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_MovieAppTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_MovieAppTestsVersionString[];
-
